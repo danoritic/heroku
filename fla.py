68201bb264803a76b59cdfee03a3f37a13a08d14
danoritic <97177939+danoritic@users.noreply.github.com>
2022-07-09 05:07:11 +0100

    Add files via upload

---
+++ fla.py
@@ -0,0 +1,21 @@
+from flask import Flask,request
+
+app=Flask(__name__)
+#/rout
+@app.route('/webhook',methods=["GET","POST"])
+def webhook():
+    req=request.get_json(silent=True,force=True)
+    fulfillmentText=""
+    query_result=req.get("queryResult")
+    if query_result.get("action")=="input.welcome":
+        # do something
+        # if required
+
+        fulfillmentText="Hi"
+    return{
+        "filfillmentText": fulfillmentText,
+        "source":"webhookdata"
+        }
+#running the app
+if __name__=="__main__":
+    app.run()
---
+++ procfile
@@ -0,0 +1,1 @@
+web: gunicorn fla:app
